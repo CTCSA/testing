0ce0bb714c02961f06ba51e88dda275f289f30b8
ckc <ckc@2u.hk>
2015-10-22 00:16:51 +0800

    Update CircleCreator.java

--- CircleCreator.java
+++ CircleCreator.java
@@ -1,17 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package p3_part3;
-
+package p3;
 
 import java.util.*;
 import java.io.*;
 
 /**
  *
- * @author a1
+ * @author ckc
  */
 public class CircleCreator extends ShapeCreator{
 
@@ -21,50 +15,17 @@ public class CircleCreator extends ShapeCreator{
     BufferedReader br = new BufferedReader(is);
 
     public Shape CreateShape()throws IOException{
- 
-            
-           // int command = Integer.parseInt(line);
+
+        
             System.out.println("Enter radius:");
             String line = br.readLine();
-            //line = br.readLine(); 
-            int r = Integer.parseInt(line);
-            //Shape s = new Circle(0, 0, r);
-            //_shapes.add(s);
         
-        return new Circle(0,0,r);
+        return new Circle(0,0,Integer.parseInt(line));
        
     }
         
         
-        /*
-        try {
-            while (cont) {
-                System.out.println("Enter command: 0 = exit, 1 = draw all shapes, 2 = create circle, 3 = create rectangle");
-                String line = br.readLine();
-                int command = Integer.parseInt(line);
-                if (command == 0) {
-                    cont = false;
-                } else if (command == 1) {
-
-                    for (int i = 0; i < _shapes.size(); i++) {
-                        ((Shape) _shapes.elementAt(i)).draw();
-                    }
-
-                } else if (command == 2) {
-
-                    System.out.println("Enter radius:");
-                    line = br.readLine();
-                    int r = Integer.parseInt(line);
-                    Shape s = new Circle(0, 0, r);
-                    _shapes.add(s);
-                }
-            }
-                }catch(Exception e){
-                    e.printStackTrace();
-                }
-            return null;
-    }*/
+//
         
         
 }
-    
