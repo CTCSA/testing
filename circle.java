77a1f6750a74f5c2d301038f5e0adc462d7c4836
ckc <ckc@2u.hk>
2015-10-22 00:15:34 +0800

    Update circle.java

--- circle.java
+++ circle.java
@@ -1,16 +1,15 @@
-/**
- * <p>Title: </p>
- * <p>Description: </p>
- * <p>Copyright: Copyright (c) 2004</p>
- * <p>Company: </p>
- * @author Clarence Lau
- * @version 1.0
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
-package p3_part3;
+package p3;
 
-
-
-public class Circle extends Shape {
+/**
+ *
+ * @author ckc
+ */
+public class Circle extends Shape{
 	protected int _radius;
 	protected int _cx; // x-coordinate of the centre
 	protected int _cy; // y-coordinate of the centre
